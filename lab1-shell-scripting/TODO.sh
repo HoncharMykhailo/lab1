fd0a95e4519227028c09145ddf5d8f12f682f021
HoncharMykhailo <123481164+HoncharMykhailo@users.noreply.github.com>
2025-09-22 16:48:39 +0300

    Update TODO.sh

--- lab1-shell-scripting/TODO.sh
+++ lab1-shell-scripting/TODO.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "TODO: implement logic"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory_path> <string>"
+    exit 1
+fi
+
+DIR="$1"
+PATTERN="$2"
+
+if [ ! -d "$DIR" ]; then
+    echo "Error: $DIR is not a directory"
+    exit 1
+fi
+
+FILES=$(find "$DIR" -type f -name "${PATTERN}*" -printf '%A@ %p\n' 2>/dev/null)
+
+if [ -z "$FILES" ]; then
+    echo "No files starting with \"$PATTERN\" found in directory $DIR."
+    exit 0
+fi
+
+echo "$FILES" | sort -nr | awk '{ $1=""; sub(/^ /,""); print }'
